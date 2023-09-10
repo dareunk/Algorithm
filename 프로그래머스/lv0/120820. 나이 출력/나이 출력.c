81a9a841d1ad4acf18a5e3f3be2be8da2d2f7a25
eunjin choi <83913407+dareunk@users.noreply.github.com>
2023-09-10 19:56:18 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int year = 0;
+    year = 2022 - age +1;
+    int answer = year;
+
+    return answer;
+}
