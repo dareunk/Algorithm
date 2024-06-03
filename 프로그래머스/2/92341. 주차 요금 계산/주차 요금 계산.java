4ba65270664c8a333d230a0aa2baa980583330ca
eunjin choi <83913407+dareunk@users.noreply.github.com>
2024-06-03 23:34:53 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.64 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -7,7 +7,8 @@ import java.util.TreeMap;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
         // 입차 이후 출차 기록 없으면 23:59 출차 간주
-        HashMap<String, Integer> map = new HashMap<>();
+        // 차량번호가 작은 순 return -> TreeMap 이용
+        TreeMap<String,Integer> map = new TreeMap<>();
         HashMap<String, Integer> countingMap = new HashMap<>();
         
         int count=0;
@@ -19,20 +20,14 @@ class Solution {
             
             if(car[2].equals("IN")) time = time * (-1);
             
-            if(!map.containsKey(car[1])) {map.put(car[1],time); countingMap.put(car[1],1);}
-            else{
-                int temp = map.get(car[1]);
-                temp = temp + time;
-                map.replace(car[1],temp);
-                countingMap.replace(car[1], countingMap.get(car[1])+1);
-            }
+            map.put(car[1],map.getOrDefault(car[1],0)+time);
+            countingMap.put(car[1],countingMap.getOrDefault(car[1],0)+1);
+            
         }
-        ArrayList<Integer> tempList = new ArrayList<>();
-  
-        // 차량번호가 작은 순
-        TreeMap<String,Integer> sortedMap = new TreeMap<>(map);
-        
-        for(String car : sortedMap.keySet()){
+    
+        int[] answer= new int[map.size()];
+        int index=0;
+        for(String car : map.keySet()){
             // 출차 기록이 없는 경우 + 23:59 
             int totaltime = 0;
             if(countingMap.get(car)%2!=0){
@@ -40,16 +35,12 @@ class Solution {
                 totaltime = map.get(car) + time;
             }else totaltime = map.get(car);
             
-            if(totaltime<=fees[0]) {tempList.add(fees[1]); continue;}
+            if(totaltime<=fees[0]) { answer[index++] = fees[1];}
             else {
                 int parkingfee = fees[1] + (int)Math.ceil((totaltime-fees[0])/(double)fees[2])*fees[3];
-                tempList.add(parkingfee);
+                answer[index++] = parkingfee;
             }
         }
-        
-        int[] answer= new int[tempList.size()];
-        int i=0;
-        for(int number:tempList) answer[i++] = number;
         
         return answer;
     }
