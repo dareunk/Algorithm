7fe7815dedd0990bd0cd7a5ef1c090da71ec764f
eunjin choi <83913407+dareunk@users.noreply.github.com>
2024-06-10 01:37:07 +0900

    [level 1] Title: 문자열 나누기, Time: 0.52 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,29 +1,21 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int firstNumber = 0;
-        int otherNumber = 0;
-
-        int i=0;
-        while(s.length()>0 && i < s.length()){
-    
-            char first = s.charAt(0);
-            if(first == s.charAt(i)) firstNumber++;
-            if(s.length() == 1) { break;}
-            else if(first != s.charAt(i)) otherNumber++;
-            i++;
+        int firstNumber =0; int otherNumber =0;
+        char first = s.charAt(0);
+        for(char c : s.toCharArray()){
+            if(firstNumber ==0 && otherNumber ==0) first = c;
             
-            if(firstNumber == otherNumber) {
-                s = s.substring(i,s.length()); 
+            if(c == first) firstNumber++;
+            else if(c!=first) otherNumber++;
+            
+            if(firstNumber == otherNumber){
+                firstNumber =0;
+                otherNumber =0;
                 answer++;
-                firstNumber =0; otherNumber=0;
-                i=0;
             }
         }
-        if(otherNumber ==0 && firstNumber >0) answer ++;
-        if(otherNumber != 0) answer++;
-     
-  
+        if(firstNumber != 0 || otherNumber != 0) answer++;
         return answer;
     }
 }
