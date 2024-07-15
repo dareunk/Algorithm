2d3fa1dcff6f3ed63b2585550172b7072e0130ef
eunjin choi <83913407+dareunk@users.noreply.github.com>
2024-07-15 17:36:48 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 19.48 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,38 +1,32 @@
-import java.util.Arrays;
 class Solution {
-    static int[] answer;
+    int[][] quad;
+    int[] answer = {0,0};
     public int[] solution(int[][] arr) {
-        answer = new int[2];
-        Arrays.fill(answer,0);
-        // size가 1인 경우 종료시킴
-        // size*size의 개수와 0또는 1의 개수가 같으면 압축
+
+        quad = arr;
         int size = arr.length;
-        compression(0,0,arr,size);
+        compression(0,0,size);
         return answer;
     }
-    public boolean isCompression(int x, int y, int[][] arr, int size, int val){
+    public boolean isCompression(int x, int y, int value, int size){
+        
         for(int i=x;i<x+size;i++){
             for(int j=y;j<y+size;j++){
-                if(arr[i][j] != val) return false;
+                if(quad[i][j] != value) return false;
             }
         }
         return true;
     }
-    public void compression(int x, int y, int[][] arr, int size){
+    public void compression(int x, int y, int size){
         
-        if(isCompression(x,y,arr,size, arr[x][y])){
-            answer[arr[x][y]]+=1;
+        if(isCompression(x,y, quad[x][y],size)){
+            answer[quad[x][y]] += 1;
             return;
         }
-       
-        //좌위
-        compression(x,y,arr,size/2);
-        //우위
-        compression(x,y+size/2,arr,size/2);
-        //좌아
-        compression(x+size/2,y,arr,size/2);
-        //우아
-        compression(x+size/2, y+size/2, arr, size/2);
         
+        compression(x,y,size/2);
+        compression(x,y+size/2,size/2);
+        compression(x+size/2,y,size/2);
+        compression(x+size/2, y+size/2, size/2);
     }
 }
